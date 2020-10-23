7c4531aac202ea30f4ed94f73a3b00773a4c44ac
eduardcaballero <eduardcaballerop@gmail.com>
2020-10-23 18:14:35 -0500

    tarea
    new app

--- settings.py
+++ settings.py
@@ -65,6 +65,12 @@ SESSION_CONFIGS = [
         'app_sequence': ['torneo'],
         'observabilidad': False,
         'meritocracia': False,
+    },
+    {
+        'name': 'tarea',
+        'display_name': 'Tarea',
+        'num_demo_participants': 1,
+        'app_sequence': ['tarea'],
     }
 ]
 
@@ -96,6 +102,11 @@ DEMO_PAGE_INTRO_HTML = """"""
 SECRET_KEY = '6k75xea8!xbc+)g%)zu&em-^(#*u38$5h1mu8o8t)7i4k5czsk'
 
 # if an app is included in SESSION_CONFIGS, you don't need to list it here
-INSTALLED_APPS = ['otree']
+INSTALLED_APPS = ['otree' , 'django_user_agents']
 
 STATIC_URL = '/static/'
+
+MIDDLEWARE_CLASSES = (
+    # other middlewares...
+    'django_user_agents.middleware.UserAgentMiddleware',
+)
