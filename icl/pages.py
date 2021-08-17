272fdd58c6109f712aaafb8744051fb841fc97be
ferleyrincon <heiner.rincon@urosario.edu.co>
2021-08-17 17:08:42 -0500

    formato porcentajes y montos

--- icl/pages.py
+++ icl/pages.py
@@ -55,7 +55,7 @@ class Decision(Page):
             'page':        page,
             'total':       total,
             'progress':    progress,
-            'sure_payoff': (self.participant.vars['icl_sure_payoffs'][page - 1])*100
+            'sure_payoff': "$"+format(int(str((self.participant.vars['icl_sure_payoffs'][page - 1])*100).split(",")[0]), ',d')
         }
 
     # set sure payoffs for next choice, payoffs, and switching row
@@ -88,10 +88,11 @@ class Results(Page):
         sure_payoff = self.player.participant.vars['icl_sure_payoffs'][choice_to_pay - 1]
 
         return {
-            'sure_payoff':     sure_payoff*100,
+            'sure_payoff':     "$"+format(int(str(sure_payoff*100).split(",")[0]), ',d'),
             'option_to_pay':   option_to_pay,
             'payoff_relevant': payoff_relevant,
-            'payoff':          self.player.in_round(choice_to_pay).payoff*100
+            'payoff':          "$"+format(int(str(self.player.in_round(choice_to_pay).payoff*100).split(",")[0]), ',d')
+            
         }
 
 
