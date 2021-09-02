ab3695a79685b78a328820e7489ecc1ee97b3afa
ferleyrincon <heiner.rincon@urosario.edu.co>
2021-09-01 19:53:53 -0500

    1

--- results/models.py
+++ results/models.py
@@ -32,7 +32,7 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    p_gender = models.IntegerField(
+    p_sex = models.IntegerField(
     choices=[
         [1, 'Hombre'],
         [2, 'Mujer'],
@@ -44,7 +44,7 @@ class Player(BasePlayer):
         [1, 'Sí'],
         [2, 'No'],
     ], label="¿Es usted estudiante?")
-    p = models.IntegerField(
+    p_job = models.IntegerField(
     choices=[
         [1,'Solo estudio'],
         [2,'Desempleado'],
@@ -56,7 +56,7 @@ class Player(BasePlayer):
         [8,'Otro'],
         [9,'No sabe']
     ], label="¿Cuál es su situación laboral actual?")
-    p4 = models.IntegerField(
+    p_educ = models.IntegerField(
     choices=[
         [1,'Ninguno'],
         [2,'Primaria'],
@@ -65,14 +65,14 @@ class Player(BasePlayer):
         [5,'Pregrado'],
         [6,'Posgrado (Especialización, Maestría, Doctorado)']
     ], label="5. ¿Cuál es el nivel educativo más alto que cursó?")
-    peduc1 = models.IntegerField(label="¿Cuántos años de educación completó en ese nivel?")
-    peduc2 = models.StringField(label="Escriba el nombre de su profesión/ocupación")
-    p7 = models.IntegerField(
+    p_educ1 = models.IntegerField(label="¿Cuántos años de educación completó en ese nivel?")
+    p_ocupation = models.StringField(label="Escriba el nombre de su profesión/ocupación")
+    p_health = models.IntegerField(
     choices=[
         [1,'Subsidiado'],
         [2,'Contributivo (incluye regímenes especiales)']
     ], label="¿A qué régimen de seguridad social en salud pertenece?")
-    p8 = models.IntegerField(
+    p_inc = models.IntegerField(
     choices=[
         [1,'Menos del Salario Mínimo Mensual (SMMLV)'],
         [2,'Entre 1 SMMLV - $ 1.500.000'],
@@ -94,12 +94,12 @@ class Player(BasePlayer):
                                             [9, "9"],
                                             [10, "10"]])
 
-    p8 = models.IntegerField(
+    p_pc = models.IntegerField(
     choices=[
         [1,'Portátil'],
         [2,'Equipo de escritorio']
     ], label="¿Qué tipo de computador usó durante la actividad?")
-    p9 = models.IntegerField(
+    p_mouse = models.IntegerField(
     choices=[
         [1,'Sí (no incluye el mouse incorporado en el computador portátil).'],
         [2,'No']
